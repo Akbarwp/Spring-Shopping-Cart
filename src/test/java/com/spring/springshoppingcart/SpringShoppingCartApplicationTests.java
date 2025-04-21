d6d13208c461e113defe753b538e97259bc20093
“Akbarwp” <akbarwp002@gmail.com>
2025-04-21 20:09:02 +0700

    chore: initialize create project Spring

---
+++ src/test/java/com/spring/springshoppingcart/SpringShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
